f7d0f30858b5c55daba460b26126aca3b696a2bb
Dominik Resl <nikicresl@gmail.com>
2017-01-31 16:36:22 +0100

    upgrade for mknachod view

--- leonardo_agenda/widget/events/models.py
+++ leonardo_agenda/widget/events/models.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 from leonardo.module.web.models import ListWidget
 import datetime
 from elephantagenda.models import Category, Event, Venue
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class EventsWidget(ListWidget):
@@ -16,32 +17,88 @@ class EventsWidget(ListWidget):
     ))
 
     category = models.ForeignKey(Category, null=True, blank=True,
-                                 help_text=_('Leave blank for all categories.'))
+                                help_text=_('Leave blank for all categories.'))
 
     venue = models.ForeignKey(Venue, null=True, blank=True,
                               help_text=_('Leave blank for all venues.'))
 
-    def get_items(self):
-
+    def get_items(self, request):
         if self.filter == 'u':
-            events = Event.objects.filter(
-                end_time__gte=datetime.datetime.now).order_by('-start_time')
+            if self.category:
+                queryset = Event.objects.filter(
+                    categories=self.category,
+                    end_time__gte=datetime.datetime.now).order_by('start_time')
+            else:
+                queryset = Event.objects.filter(
+                    end_time__gte=datetime.datetime.now).order_by('start_time')
+
+            paginator = Paginator(queryset, self.objects_per_page)
+
+            page = request.GET.get('page', None)
+
+            try:
+                events = paginator.page(page)
+            except PageNotAnInteger:
+
+                if page == "all":
+                    events = queryset
+                else:
+                    events = paginator.page(1)
+
+            except EmptyPage:
+                events = paginator.page(paginator.num_pages)
+
+            return events
+
         elif self.filter == 'p':
-            events = Event.objects.filter(
-                end_time__lte=datetime.datetime.now).order_by('start_time')
+            if self.category:
+                queryset = Event.objects.filter(
+                    categories=self.category,
+                    end_time__lte=datetime.datetime.now).order_by('-start_time')
+            else:
+                queryset = Event.objects.filter(
+                    end_time__lte=datetime.datetime.now).order_by('-start_time')
+            paginator = Paginator(queryset, self.objects_per_page)
+
+            page = request.GET.get('page', None)
+
+            try:
+                events = paginator.page(page)
+            except PageNotAnInteger:
+
+                if page == "all":
+                    events = queryset
+                else:
+                    events = paginator.page(1)
+
+            except EmptyPage:
+                events = paginator.page(paginator.num_pages)
+
+            return events
         else:
-            events = Event.objects.all().order_by('start_time')
+            if self.category:
+                events = Event.objects.filter(
+                    categories=self.category).order_by('-start_time')
+            else:
+                events = Event.objects.all().order_by('-start_time')
+
+            return events
 
         if hasattr(self.parent, 'language'):
             events = events.filter(language=self.parent.language)
 
-        if self.category:
-            events = events.filter(categories=self.category)
-
         if self.venue:
             events = events.filter(venue=self.category)
 
-        return events[:self.number]
+        return events
+
+    def get_template_data(self, request, *args, **kwargs):
+
+        data = {}
+
+        data['get_items'] = self.get_items(request)
+
+        return data
 
     class Meta:
         abstract = True
